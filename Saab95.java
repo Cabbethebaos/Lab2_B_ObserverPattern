aa23c03274f531bdd83aabaa4af27abd641b5a40
LegendarySkinnyP <73949072+LegendarySkinnyP@users.noreply.github.com>
2020-11-13 10:13:08 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -1,27 +1,27 @@
 import java.awt.*;
 
-public class Saab95 extends Car {   // klass som ärver av den abstrakta klassen Car
+public class Saab95 extends Car {   /** klass som ärver av den abstrakta klassen Car */
 
-    private boolean turboOn; /** internt tillstånd som beskriver om turbon på/av **/
+    private boolean turboOn; /** internt tillstånd som beskriver om turbon på/av */
 
-    public Saab95() {       // Konstruktor som skapar saab med 2 dörrar, röd, 125 hk, turbo av, heter Saab95, stänger av motorn
+    public Saab95() {       /** Konstruktor som skapar saab med 2 dörrar, röd, 125 hk, turbo av, heter Saab95, stänger av motorn */
         super(2, Color.red, 125, "Saab95");
         turboOn = false;
     }
 
     public void setTurboOn() {
         this.turboOn = true;
-    }       // Sätter på turbon
+    }       /** Sätter på turbon */
 
     public void setTurboOff() {
         turboOn = false;
-    }           // Stänger av turbon
+    }           /** Stänger av turbon */
 
     public boolean checkTurboOn() {
         return this.turboOn;
-    }   // Kollar om turbo på/av
+    }   /** Kollar om turbo på/av */
 
-    public double speedFactor() {                           // Metod som anger vår speed factor ökar extra om turbo på
+    public double speedFactor() {           /** Metod som anger vår speed factor ökar extra om turbo på */
         double turbo = 1;
         if (turboOn) turbo = 1.3;
         return getEnginePower() * 0.01 * turbo;
