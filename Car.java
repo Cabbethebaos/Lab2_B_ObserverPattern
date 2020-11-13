4c79c0f57ed5e59a13f40024320886b9b9d3edf1
XwayTomy <Tamas_nagy@hotmail.com>
2020-11-13 10:12:49 +0100

    tami push

--- Car.java
+++ Car.java
@@ -20,102 +20,192 @@ public abstract class Car implements Movable {      // Abstrakt klass som implem
 
     }
 
+    /**
+     *
+     * @return modellnamnet
+     */
     public String getModelName() {
         return modelName;
     }
 
+    /**
+     *
+     * @param modelName sätter modellnamn
+     */
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
 
+    /**
+     *
+     * @return Y-koordinaten
+     */
     public double getLocY() {
         return locY;
     }
+
+    /**
+     *
+     * @param locY Sätter Y-koordinaten
+     */
 
     public void setLocY(double locY) {
         this.locY = locY;
     }
 
+    /**
+     *
+     * @return X-koordinaten
+     */
     public double getLocX() {
         return locX;
     }
 
+    /**
+     *
+     * @param locX sätter x-koordinaten
+     */
     public void setLocX(double locX) {
         this.locX = locX;
     }
 
+    /**
+     *
+     * @return vilken dirr bilen pekar
+     */
     public int getDirr() {
         return dirr;
     }
 
+    /**
+     *
+     * @param dirr sätter direction
+     */
     public void setDirr(int dirr) {
         this.dirr = dirr;
     }
 
+    /**
+     *
+     * @param enginePower hk som vi vill sätta
+     */
     public void setEnginePower(double enginePower) {
         this.enginePower = enginePower;
     }
 
+    /**
+     *
+     * @param nrDoors antal dörrar vi vill sätta
+     */
     public void setNrDoors(int nrDoors) {
         this.nrDoors = nrDoors;
     }
 
+    /**
+     *
+     * @return antal dörrar som en int
+     */
     protected int getNrDoors() {
         return this.nrDoors;
-    }       // Hämtar ant dörrar
+    }
 
+    /**
+     *
+     * @return enginepower som en double
+     */
     protected double getEnginePower() {
         return this.enginePower;
-    }       // hämtar hk
+    }
 
-    protected double getCurrentSpeed() {        // Hämtar nuvarande hastighet
+    /**
+     *
+     * @return nuvarande hastighet i double
+     */
+    protected double getCurrentSpeed() {
         return this.currentSpeed;
 
     }
 
+    /**
+     *
+     * @return färgen som en color
+     */
     protected Color getColor() {
         return this.color;
-    }           // Hämtar färg
+    }
 
+    /**
+     *
+     * @param clr anger en color som ska sättas
+     */
     protected void setColor(Color clr) {
         this.color = clr;
-    }     // sätter färg
+    }
 
+    /**
+     * startar motorn och sätter currentspeed till 0.1
+     */
     protected void startEngine() {
         this.currentSpeed = 0.1;
-    }       // Startar motorn
+    }
 
+    /**
+     * stänger av motorn och sätter hastighet till 0
+     */
     protected void stopEngine() {
         this.currentSpeed = 0;
-    }           // Stryper motorn
+    }
 
-    protected abstract double speedFactor();                        // abstrakt metod som implementeras av subklasser
+    /**
+     *
+     * abstrakt metod som implementeras av subklasser
+     */
+    protected abstract double speedFactor();
 
-    public void incrementSpeed(double amount) {     // Ökar speed till högst 'enginePower'
+    /**
+     * Ökar speed till högst 'enginePower'
+     * @param amount mängden som vi vill öka med
+     */
+    public void incrementSpeed(double amount) {
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
     }
 
-    public void decrementSpeed(double amount) {     // Minskar speed till minst '0'
+    /**
+     * Minskar speed till minst '0'
+     * @param amount mängden som vi vill minska med
+     */
+    public void decrementSpeed(double amount) {
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
 
-    // TODO fix this method according to lab pm
-    public void gas(double amount) {        // Som incrementSpeed fast tar endast specifika värden
-        if (amount >= 0 && amount <= 1) // Eftersom vi endast tillåter värden mellan 0 och 1 kan bilen inte bromsas av denna funktion
+    /**
+     *  Som incrementSpeed fast tar endast specifika värden
+     * @param amount mellan 0 och 1 hur mkt vi vill gasa
+     * Eftersom vi endast tillåter värden mellan 0 och 1 kan bilen inte bromsas av denna funktion
+     */
+    public void gas(double amount) {
+        if (amount >= 0 && amount <= 1)
             incrementSpeed(amount);
         else
             throw new RuntimeException("invalid number");
     }
 
-    // TODO fix this method according to lab pm
-    public void brake(double amount) {      // Som decrementSpeed fast tar endast specifika värden
-        if (amount >= 0 && amount <= 1) // Eftersom vi endast tillåter värden mellan 0 och 1 kan bilen inte öka farten av denna funktion
+    /**
+     *  Som decrementSpeed fast tar endast specifika värden
+     * @param amount mängden vi vill bromsa med
+     *  Eftersom vi endast tillåter värden mellan 0 och 1 kan bilen inte öka farten av denna funktion
+     */
+    public void brake(double amount) {
+        if (amount >= 0 && amount <= 1)
             decrementSpeed(amount);
         else
             throw new RuntimeException("invalid number");
     }
 
-    public void move() {    // Rör sig i den riktningen som bilen pekar
+    /**
+     *  Rör sig i den riktningen som bilen pekar med nuvarande hastighet
+     */
+    public void move() {
         if (dirr == 0) {
             locY += this.getCurrentSpeed();
         } else if (dirr == 1) {
@@ -126,19 +216,30 @@ public abstract class Car implements Movable {      // Abstrakt klass som implem
 
     }
 
-    public void turnLeft() {        // Svänger vänster
+    /**
+     * Svänger vänster
+     */
+    public void turnLeft() {
         if (dirr == 0) {
             dirr = 3;
         } else dirr -= 1;
     }
 
-    public void turnRight() {       // Svänger höger
+    /**
+     *  Svänger höger
+     */
+    public void turnRight() {
         if (dirr == 3) {
             dirr = 0;
         } else dirr += 1;
     }
 
+    /**
+     *
+     * @param speed värdet som hastigheten ska sättas till
+     */
+
     public void setCurrentSpeed(double speed) {
         currentSpeed = speed;
-    }   // Sätter hastigheten till det angivna värde
+    }
 }
