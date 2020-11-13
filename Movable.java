4a00773be1565311c858eb34dff8819d24497633
LegendarySkinnyP <73949072+LegendarySkinnyP@users.noreply.github.com>
2020-11-13 10:11:04 +0100

    Update Movable.java

--- Movable.java
+++ Movable.java
@@ -1,7 +1,11 @@
-public interface Movable {      // Ett interface som fungerar som en mall för de klasser som implementerar det
-    void move();        // rörelse  metod som måste implementeras av "subklasser"
+/** Interface är en helt abstrakt klass som har i syfte att skapa metoder som inte skapar objekt.
+ * Metoderna som finns i Interface kan fås genom att implementera och objekten/body skapas i klassen som implementerar
+ * Interfaces kan man implementera flera av till skillnad från extends.
+ * Vi gör en interface Movable som innehåller metoderna move, turnDir. Dessa metoder är statiska
+ * */
+public interface Movable {   
+    void move();        /** rörelse  metod som måste implementeras av "subklasser" */ 
 
-    void turnLeft();    // sväng vänster metod som måste implementeras av "subklasser"
-
-    void turnRight();   // sväng höger metod som måste implementeras av "subklasser"
+    void turnLeft();    /** sväng vänster metod som måste implementeras av "subklasser" */ 
+    void turnRight();   /** sväng höger metod som måste implementeras av "subklasser" */ 
 }
