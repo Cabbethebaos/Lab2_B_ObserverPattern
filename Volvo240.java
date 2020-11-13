b4b5f05091bf0cc7cb4bbbefe55d05f75593b79e
LegendarySkinnyP <73949072+LegendarySkinnyP@users.noreply.github.com>
2020-11-13 10:20:45 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -1,15 +1,27 @@
 import java.awt.*;
 
-public class Volvo240 extends Car {     // klass som ärver av den abstrakta klassen Car
+/**
+ * Klass som ärver av den abstrakta klassen Car
+ */ public class Volvo240 extends Car {     
 
-    private final static double trimFactor = 1.25; // Trim nivå på volvo240
+   /**
+    * Trim nivå på volvo240
+    */
+    private final static double trimFactor = 1.25; 
 
-    public Volvo240() {         // Skapar volvo med 4 dörrar, färg: svart, 100 hk, namn: Volvo240, stoppar motorn
-        super(4, Color.black, 100, "Volvo240");
+   /**
+    * Skapar volvo med 4 dörrar, färg: svart, 100 hk, namn: Volvo240, stoppar motorn
+    */ 
+    public Volvo240() {       
+    super(4, Color.black, 100, "Volvo240");
     }
 
+   /**
+    *
+    * @return  Anger hur incrementSpeed skall öka. Returnerar en double beroende på trim och hk
+    */ 
     public double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
-    }   // Anger hur incrementSpeed skall öka. Returnerar en double beroende på trim och hk
+    }  
 
 }
